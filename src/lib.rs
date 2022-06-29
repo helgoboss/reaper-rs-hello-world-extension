fb54b295b08cbb40e7a2247f1efcc0489082e7b1
Benjamin Klum <benjamin.klum@helgoboss.org>
2022-06-29 15:06:12 +0200

    Really use the medium-level API

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,7 @@ use std::error::Error;
 
 #[reaper_extension_plugin(name = "Hello World", support_email_address = "support@example.org")]
 fn plugin_main() -> Result<(), Box<dyn Error>> {
-    let reaper = Reaper::get();
-    reaper.wake_up()?;
+    let reaper = Reaper::get().medium_reaper();
     reaper.show_console_msg("Hello from reaper-rs!\n");
     Ok(())
 }
